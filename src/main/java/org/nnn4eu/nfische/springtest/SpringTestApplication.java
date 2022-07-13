931bb8973f5246aba1f92995309721cefa53aba4
nicolekarakin <nicole.karakin@gmail.com>
2022-07-13 16:00:10 +0200

    init

---
+++ src/main/java/org/nnn4eu/nfische/springtest/SpringTestApplication.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.nfische.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nnn4eu/nfische/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.nfische.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
