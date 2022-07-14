6acd8c7204f3503012a2e77507f2609429dbb6d7
nicolekarakin <108397117+nicolekarakin@users.noreply.github.com>
2022-07-14 11:18:18 +0200

    Merge pull request #2
    * greeting ctrl added
    
    * url mapping

---
+++ src/main/java/org/nnn4eu/nfische/springtest/ctrl/Greeting.java
@@ -0,0 +1,20 @@
+package org.nnn4eu.nfische.springtest.ctrl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class Greeting {
+    @GetMapping("/a")
+    public String greetingA(){
+        return "Hy Hei Holla";
+    }
+
+    @GetMapping("/b")
+    public String greetingB(){
+        return "Hy Hei Holla";
+    }
+}
+
